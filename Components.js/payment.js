291e20a27d1a5bbce86168afad298b7698f186ef
Serwaa19 <motifrempong@gmail.com>
2024-06-26 17:21:02 +0000

    payment.js

---
+++ Components.js/payment.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { StyleSheet, View, Image, Text } from 'react-native';
+
+
+
+const Payments = ({ isDarkMode }) => {
+  const searchContainerStyle = {
+    ...styles.searchContainer,
+    backgroundColor: isDarkMode ? '#121212' : '#f0f0f0',
+  };
+  const searchBoxStyle = {
+    ...styles.searchBox,
+    backgroundColor: isDarkMode ? '#000' : '#fff',
+  };
+  const textTypeStyle = {
+    ...styles.textType,
+    color: isDarkMode ? '#fff' : '#000',
+  };
+  const searchImageStyle = {
+    ...styles.searchImage,
+  };
+
+  return (
+      <View style={searchBoxStyle}>
+      <View style={[searchContainerStyle, { marginRight: 12 }]}>
+      <Image source={require('../assets/send.png')} color={isDarkMode ? '#fff' : '#000'} top={18} size={24}/>
+        <Text style={textTypeStyle}>Sent</Text>
+      </View>
+      <View style={[searchContainerStyle, { marginHorizontal: 12}]}>
+      <Image source={require('../assets/receive.png')} color={isDarkMode ? '#fff' : '#000'} top={18} size={24}/>
+        <Text style={textTypeStyle}>Receive</Text>
+      </View >
+      <View style={[searchContainerStyle, { marginHorizontal: 12 }]}>
+        <Image source={require('../assets/loan.png')} color={isDarkMode ? '#fff' : '#000'} top={18} size={24}/>
+        <Text style={textTypeStyle}>Loan</Text>
+      </View>
+      <View style={[searchContainerStyle, { marginRight: 12}]}>
+        <Image source={require('../assets/topUp.png')} color={isDarkMode ? '#fff' : '#000'} top={18} size={24}/>
+        <Text style={textTypeStyle}>Topup</Text>
+      </View>
+    </View>
+  );
+};
+
+
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    right: 70,
+    top: 0,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    alignItems: 'center',
+  },
+  searchImage: {
+    top: 18,
+  },
+  searchBox: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginLeft: 110,
+    bottom:150,
+    width: '85%',
+  },
+  textType: {
+    marginTop: 43,
+  },
+});
+
+export default Payments;
