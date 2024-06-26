867d230fa48b4bf14b8244122cf44723fe5eaed1
Serwaa19 <motifrempong@gmail.com>
2024-06-26 17:27:20 +0000

    App.js

--- App.js
+++ App.js
@@ -1,20 +1,113 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View, Image, Text, StyleSheet } from 'react-native';
+import Home from './HomeScreen';
+import Settings from './Settings';
+import CardsScreen from './CardsScreen';
+import Statistics from './Statistics';
+import { ThemeProvider, useTheme } from './contexts/ThemeContext'; // Adjust the path as necessary
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function App() {
+  const { isDarkMode } = useTheme();
+
+  const screenOptions = {
+    tabBarShowLabel: false,
+    headerShown: false,
+    tabBarStyle: {
+      position: "absolute",
+      bottom: 0,
+      elevation: 0,
+      backgroundColor: isDarkMode ? "#121212" : "#fff", // Change background color based on theme
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!     /</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOptions} initialRouteName="Home" style={styles.Tab}>
+        <Tab.Screen name="Home" component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View style={styles.center}>
+                <View style={styles.iconContainer}>
+                  <Image source={require('./assets/home.png')} style={styles.icon} />
+                </View>
+                <Text style={[styles.iconText, { color: isDarkMode ? '#fff' : '#000' }]}>Home</Text>
+              </View>
+            )
+          }} />
+        <Tab.Screen name="MyCards" component={CardsScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View style={styles.center}>
+                <View style={styles.iconContainer}>
+                  <Image source={require('./assets/myCards.png')} style={styles.icon} />
+                </View>
+                <Text style={[styles.iconText, { color: isDarkMode ? '#fff' : '#000' }]}>My Cards</Text>
+              </View>
+            )
+          }}
+        />
+        <Tab.Screen name="Statistics" component={Statistics}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View style={styles.center}>
+                <View style={styles.iconContainer}>
+                  <Image source={require('./assets/statictics.png')} style={styles.icon} />
+                </View>
+                <Text style={[styles.iconText, { color: isDarkMode ? '#fff' : '#000' }]}>Statistics</Text>
+              </View>
+            )
+          }}
+        />
+        <Tab.Screen name="Settings" component={Settings}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View style={styles.center}>
+                <View style={styles.iconContainer}>
+                  <Image source={require('./assets/settings.png')} style={styles.icon} />
+                </View>
+                <Text style={[styles.iconText, { color: isDarkMode ? '#fff' : '#000' }]}>Settings</Text>
+              </View>
+            )
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default function MainApp() {
+  return (
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  contentContainer: {
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingBottom: 40,
+    paddingTop: 0,
   },
+  Tab: {
+    width: '50%'
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  icon: {
+    width: 24,
+    height: 24,
+  },
+  iconText: {
+    marginTop: 4,
+    fontSize: 12,
+  }
 });
+
